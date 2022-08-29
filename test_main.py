85104246a0402fe0bb3348f53ce071f6f65b7495
elon <elon4856@gmail.com>
2022-08-29 16:18:47 +0900

    기본세팅

---
+++ test_main.py
@@ -0,0 +1,15 @@
+# test_main.py
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+
+    message = response.json()["message"]
+
+    assert len(message) < 40
